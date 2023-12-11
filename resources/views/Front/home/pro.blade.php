535bc056e07ea5078649bf2a524b59eb0e8916f8
dhruba-utsha <utshasen676@gmail.com>
2023-12-12 01:53:24 +0600

    inserting code 2

---
+++ resources/views/Front/home/pro.blade.php
@@ -0,0 +1,35 @@
+@extends('Front.layouts.app')
+@section('title') @lang('app.app-description') @endsection
+@section('content')
+<div class="d-flex justify-content-center" style="margin-top: 150px;">
+
+</div>
+<div class="container-xxl text-center">
+
+<div class="h1 my-3">
+"Step into Excellence:  Introducing L4L Pro - Elevate Your Experience Today!"
+</div>
+
+<div class="h1 my-3">
+"L4L PRO" Home page
+</div>
+<img class="img-fluid" src="{{asset('img/pro/1.png')}}" alt="">
+
+<div class="h1 my-3">
+"L4L PRO" Dashboard page
+</div>
+<img class="img-fluid" src="{{asset('img/pro/5.jpg')}}" alt="">
+
+<div class="h1 my-3">
+About Pro version of Look For Land
+</div>
+<p class="fs-4">The Look For Land PRO version name is L4L Pro and the website has new private logo that is designed for this website. The website can change background theme dark and light or auto choose your browsers or devices bacground theme and this website more responsive and dynamic. In PRO version website users after register can add estate and service. The users have statuses and there are "Start-up", "Professional" and "Enterprise". Start-up is free they can post only 5 posts. Professionals they can post 20 posts and professionals posts automatically adding to VIP post that ordering in first place and top-right automatically appears badge that Premium with yellow color and finally Enterprise users they can post infinity posts and they posts are ordered first all other posts and this also appears top-right  automatically badge that Premium with yellow color and admin first checks and answers the enterprise users and after checks professional users and finally checks start-up users posts. *If you want to know more about this PRO version site you can contact us* @Look4Land
+</p>
+
+<img class="img-fluid" width="500" src="{{asset('img/pro/L4Lpro.png')}}" alt="">
+<div class="h1">
+            <i class="bi bi-envelope-fill"></i> <a class="color-a">Look4Land@gmail.com</a><br>
+            <i class="bi bi-link"></i> <a class="color-a">Look4Land</a><br>
+        </div>
+</div>
+@endsection
